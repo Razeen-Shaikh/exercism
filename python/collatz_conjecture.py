9460653da780f1e15530c9110ffad935433a55e7
Razeen Shaikh <34880700+Razeen-Shaikh@users.noreply.github.com>
2024-03-24 12:38:13 +0530

    collatz conjecture

---
+++ python/collatz_conjecture.py
@@ -0,0 +1,20 @@
+def steps(number):
+    """
+    This function calculates the number of steps required to reach 1
+    based on the Collatz conjecture for a given positive integer.
+    
+    Parameters:
+        number (int): The positive integer for which steps are to be calculated
+    
+    Returns:
+        int: The number of steps required to reach 1
+    """
+    count = 0
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+    while number != 1:
+        if number % 2 == 0:
+            number //= 2
+        else: number = 3 * number + 1
+        count += 1
+    return count
