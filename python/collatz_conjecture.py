83148d63f25b681b34288be79e7d67659314d6e7
Razeen Shaikh <34880700+Razeen-Shaikh@users.noreply.github.com>
2024-03-29 14:51:23 +0530

    Update collatz_conjecture.py

--- python/collatz_conjecture.py
+++ python/collatz_conjecture.py
@@ -9,12 +9,13 @@ def steps(number):
     Returns:
         int: The number of steps required to reach 1
     """
-    count = 0
     if number <= 0:
         raise ValueError("Only positive integers are allowed")
+    count = 0
     while number != 1:
         if number % 2 == 0:
             number //= 2
-        else: number = 3 * number + 1
+        else:
+            number = 3 * number + 1
         count += 1
     return count
